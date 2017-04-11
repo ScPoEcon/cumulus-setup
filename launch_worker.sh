0c41fec9ae98ac1cea899f9a46a515a99cbece29
florian oswald <florian.oswald@gmail.com>
2017-04-11 23:15:18 +0200

    dd

--- launch_worker.sh
+++ launch_worker.sh
@@ -5,18 +5,18 @@
 
 # task 1: setup SSH between login and worker nodes
 
-workers = (vm3-8core vm4-8core vm5-8core vm6-8core)
+declare -a workers = (vm3-8core vm4-8core vm5-8core vm6-8core)
 for i in "${workers[@]}"
 do
 	echo "setting up SSH on worker $i"
-	cat ~/.ssh/id_rsa.pub | ssh root@$i 'cat >> .ssh/authorized_keys'
+	cat ~/.ssh/id_rsa.pub | ssh root@"$i" 'cat >> .ssh/authorized_keys'
 done
 
 # task 2: install software
 for i in "${workers[@]}"
 do
 	echo "install on worker $i"
-	scp install.sh root@$i
+	scp install.sh root@"$i"
 	ssh $i 'chmod 755 install.sh'
 	ssh $i './install.sh'
 	ssh $i 'rm ./install.sh'
