415f8809f28a059d5c034b8cb9176181edfe0d90
Florian Oswald <florian.oswald@gmail.com>
2017-04-24 17:19:36 +0200

    added shared filesystem

---
+++ nfs.sh
@@ -0,0 +1,28 @@
+# have a shared filed system
+# run on master
+apt install nfs-kernel-server
+# this will export the directory /root/git to all nodes listed
+echo -e "/root/git 10.20.35.6(sync)  
+/root/git 10.20.35.7(sync) 
+/root/git 10.20.35.8(sync)  
+/root/git 10.20.35.9(sync)" | \
+cat >> /etc/exports
+
+exportfs -a
+
+echo "starting nfs server"
+systemctl start nfs-kernel-server.service
+echo "done starting nfs server"
+
+
+echo "starting nfs clients"
+
+# run on slaves
+declare -a workers=(vm3-8core vm4-8core vm5-8core vm6-8core)
+for i in "${workers[@]}"
+do
+	echo "on worker $i"
+	ssh root@"$i" apt install nfs-common
+	ssh root@"$i" mount 10.20.35.5:/root/git /root/git
+	echo "done."
+done
