374ca8ef1987054cd71502c0a1d491ddf54504e8
Florian Oswald <florian.oswald@gmail.com>
2017-04-26 11:53:16 +0200

    t

---
+++ test2.sh
@@ -0,0 +1,28 @@
+declare -a workers=(vm3-8core vm4-8core vm5-8core)
+for i in "${workers[@]}"
+do
+	echo "working on worker $i"
+	# ssh root@"$i" apt install nfs-common
+	# ssh root@"$i" umount /root/git && umount /root/.julia
+	ssh root@"$i" /bin/bash << 'EOT'
+	echo "These commands will be run on: $( uname -n )"
+	apt-get install nfs-common
+	echo "mounting now"
+	mount 10.20.35.11:/root /root
+	mount 10.20.35.11:/usr/local /usr/local
+	mkdir -p /apps
+	mount 10.20.35.11:/apps /apps
+	mount
+	echo "They are executed by: $( whoami )"
+	echo "doing stuff now"
+	mkdir -p testing
+	cd testing
+	touch test.file
+	echo "this is the file content" | cat >> test.file
+	echo "see whats in it:"
+	cat test.file
+	cd ..
+	rm -rf testing
+	echo "done"
+EOT
+done
