ef505af4fce6fa66df9193108820e1819d729af4
florian oswald <florian.oswald@gmail.com>
2017-04-26 11:00:15 +0200

    ssh on rest

--- ssh.sh
+++ ssh.sh
@@ -2,14 +2,26 @@
 
 # create new key pair on login node
 # don't put in a password when asked.
-ssh-keygen -t rsa
+# ssh-keygen -t rsa
 
-# task 1: setup SSH between login and worker nodes
+# # task 1: setup SSH between login and worker nodes
 
-declare -a workers=(vm3-8core vm4-8core vm5-8core vm6-8core)
+# declare -a workers=(vm3-8core vm4-8core vm5-8core vm6-8core)
+# for i in "${workers[@]}"
+# do
+# 	echo "setting up SSH on worker $i"
+# 	cat ~/.ssh/id_rsa.pub | ssh root@"$i" 'cat >> .ssh/authorized_keys'
+# 	echo "done."
+# done
+
+
+declare -a workers=(vm6-8core vm7-8core vm8-8core vm9-8core vm10-8core)
 for i in "${workers[@]}"
 do
-	echo "setting up SSH on worker $i"
+	echo "working on worker $i"
 	cat ~/.ssh/id_rsa.pub | ssh root@"$i" 'cat >> .ssh/authorized_keys'
-	echo "done."
+	# ssh root@"$i" apt install nfs-common
+	# ssh root@"$i" umount /root/git && umount /root/.julia
+	echo "done"
+EOT
 done
