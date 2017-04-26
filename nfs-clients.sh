219cb8de2cb20a4fcca6e73d224d08a29a18aec0
Florian Oswald <florian.oswald@gmail.com>
2017-04-26 11:45:25 +0200

    new

--- nfs-clients.sh
+++ nfs-clients.sh
@@ -1,31 +1,3 @@
-
-
-
-# setup a shared filed system
-
-# after https://www.digitalocean.com/community/tutorials/how-to-set-up-an-nfs-mount-on-ubuntu-16-04
-
-# run on master
-apt install nfs-kernel-server
-# this will export the directory /root to all nodes listed
-rm -rf /etc/exports
-echo -e "
-/root      10.20.35.6/16.16.16.0(rw,sync,no_root_squash)
-/usr/local 10.20.35.6/16.16.16.0(ro,sync,no_root_squash)
-/apps      10.20.35.6/16.16.16.0(ro,sync,no_root_squash)" | \
-cat >> /etc/exports
-
-exportfs -a
-
-echo "starting nfs server"
-systemctl start nfs-kernel-server.service
-echo "done starting nfs server"
-
-
-echo "starting nfs clients"
-echo "CAUTION this does not work via SSH?"
-
-# run on slaves
 declare -a workers=(vm4-8core vm5-8core vm6-8core vm7-8core vm8-8core vm9-8core vm10-8core)
 for i in "${workers[@]}"
 do
